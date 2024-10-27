25458ddf074b8e06c934bedaa8befdbd3b0d4407
Ben Beattie-Hood <bbeattiehood@atlassian.com>
2024-10-27 19:49:31 +1100

    Corrected DeepMerge

--- src/06-DeepMerge.ts
+++ src/06-DeepMerge.ts
@@ -1,4 +1,4 @@
-const deepMergeRecords_untyped = (...args: any[]): any => {
+export const deepMergeRecords_untyped = (...args: any[]): any => {
     //#region Code
     const isMergableRecord = (o: any): o is Record<keyof any, any> =>
         typeof o === "object" &&
@@ -31,72 +31,132 @@ const deepMergeRecords_untyped = (...args: any[]): any => {
     //#endregion
 };
 
-const result = deepMergeRecords_untyped(
+const result_example = deepMergeRecords_untyped(
     {
-        componentType: "primary-pressable",
-        style: {
-            backgroundColor: "#CD2E96",
-            color: "white",
-        }
+        backgroundColor: "#CD2E96",
+        color: "white",
+        ":hover": {
+            backgroundColor: "#A40061",
+        },
     },
     {
-        componentType: "dev-pressable",
-        style: {
-            fontWeight: 'bold',
-            fontFamily: 'monospace',
-        },
-        behaviour: {
-            onPress: () => console.log("Pressed"),
-        }
+        fontWeight: "bold",
+        fontFamily: "monospace",
     },
     {
-        componentType: "dev-theme",
-        style: {
-            fontFamily: 'monospace',
+        fontFamily: '"Fira Code"',
+        ":hover": {
+            outline: "solid 1px white",
         },
-    }
+    },
 );
 
+/*
+Output should be:
+const result: {
+  backgroundColor: '#CD2E96',
+  color: 'white',
+  fontWeight: 'bold',
+  fontFamily: '"Fira Code"',
+  ':hover': {
+    backgroundColor: '#A40061',
+    outline: 'solid 1px white'
+  }
+}
+*/
 
+//#region First, let's merge two records
+//#region Example
+type AnyRecord_Example = Record<keyof any, unknown>;
 
-
-
-export type DeepMergeTwoRecords<
-    A extends Record<keyof any, any>,
-    B extends Record<keyof any, any>,
-> = A extends object
-    ? B extends object
+type DeepMergeTwoRecords_Example<
+    A extends AnyRecord_Example,
+    B extends AnyRecord_Example,
+> = [A, B] extends [Record<infer AKey, unknown>, Record<infer BKey, unknown>]
     ? {
-        [K in keyof A | keyof B]: K extends keyof A
-        ? K extends keyof B
-        ? A[K] extends object
-        ? B[K] extends object
-        ? DeepMergeTwoRecords<A[K], B[K]>
-        : B[K]
-        : B[K]
-        : A[K]
-        : // this latter part shouldn't really be needed, but
-        // TS is having difficulty inferring here that K must
-        // always be a keyof B 🤷‍♂️
-        K extends keyof B
-        ? B[K]
-        : never;
-    }
-    : B
-    : B;
+          [K in AKey | BKey]: K extends AKey
+              ? K extends BKey
+                  ? A[K] extends AnyRecord_Example
+                      ? B[K] extends AnyRecord_Example
+                          ? DeepMergeTwoRecords_Example<A[K], B[K]>
+                          : B[K]
+                      : B[K]
+                  : A[K]
+              : K extends BKey
+                ? B[K]
+                : never;
+      }
+    : never;
+//#endregion
 
-export type DeepMergeMultipleRecords<
-    A extends Record<keyof any, any>,
-    Tail extends [...any[]],
-> = Tail extends [infer B extends Record<keyof any, any>]
-    ? DeepMergeTwoRecords<A, B>
-    : Tail extends [infer B extends Record<keyof any, any>, ...infer NextTail]
-    ? DeepMergeTwoRecords<A, DeepMergeMultipleRecords<B, NextTail>>
-    : A;
-
-const deepMergeRecords = <
-    A extends Record<keyof any, any>,
-    B extends [A, ...any[]],
+const deepMergeTwoRecords = <
+    A extends AnyRecord_Example,
+    B extends AnyRecord_Example,
 >(
-    ...args: B
-): DeepMergeMultipleRecords<A, B> => { };
+    a: A,
+    b: B,
+): DeepMergeTwoRecords<A, B> => {};
+
+const testOfMergingTwoRecords = deepMergeTwoRecords(
+    {
+        backgroundColor: "#CD2E96",
+        color: "white",
+        ":hover": {
+            backgroundColor: "#A40061",
+        },
+    },
+    {
+        fontWeight: "bold",
+        fontFamily: "monospace",
+    },
+);
+
+//#region Now, let's merge N records
+//#region Example
+export type DeepMergeMultipleRecords<Records extends AnyRecord_Example[]> =
+    Records extends []
+        ? never
+        : Records extends [infer A extends AnyRecord_Example]
+          ? A
+          : Records extends [
+                  infer A extends AnyRecord_Example,
+                  infer B extends AnyRecord_Example,
+              ]
+            ? DeepMergeTwoRecords_Example<A, B>
+            : Records extends [
+                    infer A extends AnyRecord_Example,
+                    infer B extends AnyRecord_Example,
+                    ...infer Rest extends AnyRecord_Example[],
+                ]
+              ? DeepMergeTwoRecords_Example<
+                    A,
+                    DeepMergeMultipleRecords<[B, ...Rest]>
+                >
+              : never;
+//#endregion
+
+const deepMergeRecords = <const Records extends AnyRecord_Example[]>(
+    ...args: Records
+): DeepMergeMultipleRecords<Records> => {};
+
+const testOfMergingMultipleRecords = deepMergeRecords(
+    {
+        backgroundColor: "#CD2E96",
+        color: "white",
+        ":hover": {
+            backgroundColor: "#A40061",
+        },
+    },
+    {
+        fontWeight: "bold",
+        fontFamily: "monospace",
+    },
+    {
+        fontFamily: '"Fira Code"',
+        ":hover": {
+            outline: "solid 1px white",
+        },
+    },
+);
+//#endregion
+//#endregion
