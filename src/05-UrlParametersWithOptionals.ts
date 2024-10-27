0a44c379c683a73940f7a4cdcc03455dc1b73db6
Ben Beattie-Hood <bbeattiehood@atlassian.com>
2024-10-27 19:50:43 +1100

    Improvements to UrlParametersWithOptionals

--- src/05-UrlParametersWithOptionals.ts
+++ src/05-UrlParametersWithOptionals.ts
@@ -1,8 +1,6 @@
 export type ParamsOf<Url extends string> = Extract<
-    Url extends `${infer Head}/${infer Rest}`
-    ? Head | ParamsOf<Rest>
-    : Url extends ""
-    ? never
+    Url extends `${infer Head}/${infer Rest}` ? Head | ParamsOf<Rest>
+    : Url extends "" ? never
     : Url,
     `:${string}`
 >;
@@ -46,14 +44,14 @@ type ParamsWithOptionalOf_v1<Url extends string> =
     Url extends `${infer Head}(${infer HeadOptional})${infer Rest}`
     ? MergeParamSplits_v1<
         {
-            required: ParamsOf<Head>,
-            optional: ParamsOf<HeadOptional>
+            required: ParamsOf<Head>;
+            optional: ParamsOf<HeadOptional>;
         },
         ParamsWithOptionalOf_v1<Rest>
     >
     : {
-        required: ParamsOf<Url>,
-        optional: never
+        required: ParamsOf<Url>;
+        optional: never;
     };
 
 type ResultWithOptional_v1 =
@@ -92,12 +90,16 @@ type ResultWithOptional_v2_Example =
     ParamsWithOptionalOf_v2_Example<"api/person/:personId(/address/:addressId)">;
 //#endregion
 
-//#region Split that into two separate Record types, intersect them, and assign that to our new ParamsOfWithOptional type
+//#region Use that parsed type within two separate Record types, which we can intersect (&), and assign to a shorthand new 'ParamsOfWithOptional' type that we can use
 //#region Example
-type RequiredAndPartialRecord<T extends RequiredAndOptional_Example<string, string>, Value> =
-    Record<T["required"], Value>
-    & Partial<Record<T["optional"], Value>>;
-type ParamsOfWithOptional<Url extends string> = RequiredAndPartialRecord<ParamsWithOptionalOf_v2_Example<Url>, string>;
+type RequiredAndPartialRecord<
+    T extends RequiredAndOptional_Example<string, string>,
+    Value,
+> = Record<T["required"], Value> & Partial<Record<T["optional"], Value>>;
+type ParamsOfWithOptional<Url extends string> = RequiredAndPartialRecord<
+    ParamsWithOptionalOf_v2_Example<Url>,
+    string
+>;
 //#endregion
 
 function interpolateUrlWithOptionals<Url extends string>(
